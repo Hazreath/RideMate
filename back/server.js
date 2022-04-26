81c57f9fe7afbe2b184bf50bd0d20b1cff0f8cdf
Hazreath <benjamincamp.it@gmail.com>
2022-04-26 15:35:15 +0200

    backend creation + basic login and profile infos get

---
+++ back/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 3001
+
+mongoose.connect('mongodb+srv://Hazreath:zFHBKgxe7ezsBDvl' +
+  '@ridemate.2poar.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        {   useNewUrlParser: true,
+            useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((e ) => console.log('Connexion à MongoDB échouée ! : ' + e));
+
+
+
+// middlewares
+app.use((req,res,next) => { // HEADERS
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next()
+})
+
+app.use(express.json())// req.body etc
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('banjour!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
