4b7e4b3f1078df989e6e64a49c75d52ee9280955
Hazreath <benjamincamp.it@gmail.com>
2022-05-11 18:29:59 +0200

    gitignore

--- back/settings.js
+++
@@ -1,6 +0,0 @@
-class Settings {
-    static DB_URL = 'mongodb+srv://Hazreath:zFHBKgxe7ezsBDvl' +
-    '@ridemate.2poar.mongodb.net/RideMate?retryWrites=true&w=majority'
-}
-
-module.exports = Settings
--- playground/src/models/Platform.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const schema = mongoose.Schema({
-    // auto _id
-    name: {type: String, required:true, unique:true},
-})
-
-module.exports = mongoose.model('Platform',schema)
