a5c10086fd38bb5614e08a0e821b70aedba6bf38
Hazreath <benjamincamp.it@gmail.com>
2022-10-01 14:18:16 +0200

    fix

--- back/server.js
+++ back/server.js
@@ -1,4 +1,4 @@
-const Settings = require("./settings");
+// const Settings = require("./settings");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
