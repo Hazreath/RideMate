c2e8ca094435d97ba0a0c539ff6b79745d624672
Hazreath <benjamincamp.it@gmail.com>
2022-10-03 18:21:19 +0200

    debug env vars

--- back/server.js
+++ back/server.js
@@ -14,7 +14,11 @@ const tricksRoutes = require("./routes/tricks");
 const app = express();
 require("dotenv").config();
 // const port = 3001;
-console.log(process.env.BACK_PORT);
+console.log(process.env);
+if (process.env.DEBUG_ENV_VARS === "true") {
+    console.log("DEBUG_ENV_VARS");
+    console.log(process.env);
+}
 const port = process.env.BACK_PORT || 3000;
 
 /**
